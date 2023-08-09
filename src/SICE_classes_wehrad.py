41484e2f16956c97c1aad6aa7ef1d8c24fea4dc1
Rasmus Bahbah Nielsen <114926145+RasmusBahbah@users.noreply.github.com>
2023-08-09 13:38:34 +0200

    Adding testing, prediction, exporting as tiff, and some first attempt of robust statistics on the dateset

--- src/SICE_classes_wehrad.py
+++ src/SICE_classes_wehrad.py
@@ -18,17 +18,78 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_classification
 from sklearn import svm
+import xarray as xr
+from rasterio.transform import Affine
+from pyproj import CRS as CRSproj
+from scipy.spatial import KDTree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss
+
+def opentiff(filename):
+    
+    "Input: Filename of GeoTIFF File "
+    "Output: xgrid,ygrid, data paramater of Tiff, the data projection"
+   
+    da = xr.open_rasterio(filename)
+    proj = CRSproj.from_string(da.crs)
+
+
+    transform = Affine(*da.transform)
+    elevation = np.array(da.variable[0],dtype=np.float32)
+    nx,ny = da.sizes['x'],da.sizes['y']
+    x,y = np.meshgrid(np.arange(nx,dtype=np.float32), np.arange(ny,dtype=np.float32)) * transform
+
+    da.close()
+   
+    return x,y,elevation,proj
+
+def exporttiff(x,y,z,crs,filename):
+    
+    "Input: xgrid,ygrid, data paramater, the data projection, export path, name of tif file"
+   
+    resx = (x[0,1] - x[0,0])
+    resy = (y[1,0] - y[0,0])
+    transform = Affine.translation((x.ravel()[0]),(y.ravel()[0])) * Affine.scale(resx, resy)
+    
+    if resx == 0:
+        resx = (x[0,0] - x[1,0])
+        resy = (y[0,0] - y[0,1])
+        transform = Affine.translation((y.ravel()[0]),(x.ravel()[0])) * Affine.scale(resx, resy)
+    
+    with rasterio.open(
+    filename,
+    'w',
+    driver='GTiff',
+    height=z.shape[0],
+    width=z.shape[1],
+    count=1,
+    dtype=z.dtype,
+    crs=crs,
+    transform=transform,
+    ) as dst:
+        dst.write(z, 1)
+    
+    dst.close()
+    
+    return None 
+
 
 # ## change to your system's login name to change dir for local work
 if os.getlogin() == "jason":
     base_path = "/Users/jason/Dropbox/S3/SICE_classes/"
 if os.getlogin() == "adrien":
     base_path = "/home/adrien/EO-IO/SICE_classes/"
+    
+current_path = os.getcwd()
+base_path = os.path.abspath('..')
+
 os.chdir(base_path)
 
 # relative paths
 path_raw = "./SICE_rasters/"
 path_ROI = "./ROIs/"
+
+raster_path = base_path + path_raw
 
 
 def read_S3(fn):
@@ -51,6 +112,9 @@ n_bands = len(bands)
 ni = 5424
 nj = 2959  # all greenland raster dimensions
 
+band = bands[0]
+x_grid,y_grid,dummy,proj = opentiff(f"{raster_path}2019-08-02_{band}.tif")
+
 # initialise array to contain band data
 Xs = np.zeros(((n_bands, ni, nj)))
 
@@ -62,11 +126,12 @@ for i, band in enumerate(bands):
     Xs[i, :, :] = r
 
 # %% test multi band array loading
-plt.imshow(Xs[3, :, :])
+plt.imshow(Xs[1, :, :])
+plt.show()
 # %%
 # load lables into a 3 D array
 
-rois = ["bright_ice", "dark_ice", "dry_snow", "flooded_snow", "melted_snow", "red_snow"]
+rois = ["dry_snow", "melted_snow", "flooded_snow","red_snow","bright_ice", "dark_ice"]
 
 n_features = len(rois)
 
@@ -89,21 +154,25 @@ for i, roi in enumerate(rois):
         plt.hist(temp)  # plt.imshow(r)
         plt.title(roi)
         plt.show()
+
+    
+
 # %% test multi label array loading
 plt.imshow(LABELS[2, :, :])
 
-# %% format inputs and labels
+# %% format inputs and labels and make test dataset
 
 labels = []
 S3_data_for_labels_all = []
 
+# Training Data with Label
 for i, roi in enumerate(rois):
     mask = np.isfinite(LABELS[i, :, :])
 
     S3_data_for_labels = np.vstack(
         [read_S3(f"{path_raw}2019-08-02_{band}.tif")[mask] for band in bands]
     ).T
-
+    
     S3_data_for_labels_all.append(S3_data_for_labels)
 
     labels.append(np.repeat(i, S3_data_for_labels.shape[0]))
@@ -120,7 +189,171 @@ no_nan_mask = np.where(np.sum(~np.isnan(inputs_for_svm), axis=1) == 4)[0]
 inputs_for_svm = inputs_for_svm[no_nan_mask, :]
 labels_for_svm = labels_for_svm[no_nan_mask]
 
-# %% train SVM
 
-clf = svm.SVC(decision_function_shape="ovo")
-clf.fit(inputs_for_svm, labels_for_svm)
+### Splitting dataset into  Training and Test set ###
+# When there is more labeled data, it would be good to have a another date as test set #
+
+data_train, data_test, label_train, label_test \
+     = train_test_split(inputs_for_svm, labels_for_svm, test_size=0.33, random_state=42) 
+
+### Maybe introduce robust statistics ### 
+# ml_est = sum(w_i * d_i) / sum(w_i)
+# if samples are gaussian w_i = 1 
+### reweighting algorithm using Huber weights, until convergence ###
+
+def huber_w(w,d,sigma):
+    
+    break_p = 1.345  
+    ml_est = sum(w * d) / sum(w)
+    eps = (d - ml_est) / sigma 
+    
+    w[eps > break_p] = break_p/eps[eps > break_p]
+    w[abs(eps) <= break_p] = 1
+    w[eps < -break_p] = -break_p/eps[eps < -break_p]
+    
+    return w 
+
+
+no_i = np.arange(50) #Number of iterations
+w_all = np.ones_like(data_train)
+
+for n in np.arange(n_features):
+    for b in np.arange(len(bands)):
+        w = w_all[:,b][label_train == n]
+        d = data_train[:,b][label_train == n]
+        sigma = np.std(d)
+        for i in no_i:
+            w = huber_w(w,d,sigma)
+        w_all[:,b][label_train == n] = w 
+
+w_samples = np.array([np.nanmean(w) for w in w_all])        
+ 
+
+# %% Find Regularization Parameter C
+
+alpha = np.arange(1,10,0.1)
+
+lloss_svc = np.ones_like(alpha)
+
+for i,a in enumerate(alpha):
+  print(f'Finding Solution for Alpha Value no. {i}')
+  C = 1 / a
+  clf = svm.SVC(C = C, decision_function_shape="ovo",probability = True)
+  clf.fit(data_train, label_train,sample_weight=w_samples)  
+  label_prob = clf.predict_proba(data_test)
+  lloss_svc[i] = log_loss(label_test,label_prob)
+  
+  #score_svc[i] = clf.sore(data_test,label_test)
+
+plt.figure(figsize=(14,8))
+ax=plt.gca()
+ax.plot(alpha,lloss_svc)
+
+lloss_min_val = np.nanmin(lloss_svc)
+alp_lloss = alpha[lloss_svc==lloss_min_val]
+lloss_min = lloss_svc[lloss_svc==lloss_min_val]
+
+ax.scatter(alp_lloss,lloss_min,c='red',s=12)
+
+plt.xticks(fontsize = 26)
+plt.yticks(fontsize = 26)
+plt.xlabel('Alpha Value',fontsize = 26)
+plt.ylabel('svc Log Loss',fontsize = 26)
+
+plt.show()
+
+# scores = defaultdict(list)
+# 
+#     clf.fit(X_train, y_train)
+#     y_prob = clf.predict_proba(X_test)
+#     y_pred = clf.predict(X_test)
+#     scores["Classifier"].append(name)
+
+#     for metric in [brier_score_loss, log_loss, roc_auc_score]:
+#         score_name = metric.__name__.replace("_", " ").replace("score", "").capitalize()
+#         scores[score_name].append(metric(y_test, y_prob[:, 1]))
+
+#     for metric in [precision_score, recall_score, f1_score]:
+#         score_name = metric.__name__.replace("_", " ").replace("score", "").capitalize()
+#         scores[score_name].append(metric(y_test, y_pred))
+
+#     score_df = pd.DataFrame(scores).set_index("Classifier")
+#     score_df.round(decimals=3)
+
+# score_df
+
+# %% train and test SVM 
+
+# alpha = np.arange(1,10,0.01)
+# 
+# for a in alpha:
+#   C = 1 / a
+#   clf = svm.SVC(C = C, decision_function_shape="ovo")
+#   clf.fit(inputs_for_svm, labels_for_svm,sample_weight=w_samples)
+#
+alpha = 0
+
+
+if alpha == 0:
+    C = 1
+else:
+    C = 1 / alpha
+
+clf = svm.SVC(C = C, decision_function_shape="ovo",probability = True)
+clf.fit(inputs_for_svm, labels_for_svm,sample_weight=w_samples)
+
+model_params = clf.get_params()
+
+
+# %% Test SVM 
+
+score_svc = clf.score(data_test,label_test)
+label_prob = clf.predict_proba(data_test)
+lloss_svc = log_loss(label_test,label_prob)
+
+print(f"Accuracy of Model {score_svc}")
+print(f"Log Loss of Model {lloss_svc}")
+
+# %% Data to Classify
+
+S3_data_for_predict_all = []
+
+
+# Data to predict a Label
+
+mask_predict = ~np.isfinite(LABELS[:, :, :])
+mask_predict = np.array([[all(mask_predict[:,n,m]) for m in np.arange(nj)] for n in np.arange(ni)])
+
+x_coor = x_grid[mask_predict]
+y_coor = y_grid[mask_predict]
+
+S3_data_for_predict = np.vstack(
+     [read_S3(f"{path_raw}2019-08-02_{band}.tif")[mask_predict] for band in bands]
+ ).T
+
+
+no_nan_mask = np.where(np.sum(~np.isnan(S3_data_for_predict), axis=1) == 4)[0]
+S3_data_for_predict_all =  S3_data_for_predict[no_nan_mask, :]
+
+x_coor = x_coor[no_nan_mask]
+y_coor = y_coor[no_nan_mask]
+
+# %% Model Predict
+
+labels_predict = clf.predict(S3_data_for_predict_all)
+
+# %% Regridding to SICE Grid and saving as geotiff
+
+tree = KDTree(np.c_[x_coor.ravel(),y_coor.ravel()])   
+datagrid = np.ones_like(x_grid) * np.nan
+
+for i,(xmid,ymid) in enumerate(zip(x_grid.ravel(),y_grid.ravel())):     
+        
+        dd, ii = tree.query([xmid,ymid],k = 1,p = 2)
+        ii = ii[dd<500]
+        
+        if len(ii) > 0:
+            datagrid.ravel()[i] = labels_predict.ravel()[ii]
+
+file = raster_path + os.sep + '2019_08_02_labels_v2.tif'
+exporttiff(x_grid,y_grid,datagrid,proj,file)
